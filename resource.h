52df9d3050729b9d2b6f0485ac6c0be966b541f2
RadAd <adamgates84+github@gmail.com>
2024-07-10 13:27:36 +1000

    Add new shortcut

--- resource.h
+++ resource.h
@@ -26,7 +26,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_COMMAND_VALUE         40017
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
